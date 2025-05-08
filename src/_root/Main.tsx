2df42816c18d5551386b512f4836f41795622497
Mateus Loubach <mcesarloubach@gmail.com>
2025-05-08 14:33:10 -0300

    chore: rollback for release;

--- src/_root/Main.tsx
+++ src/_root/Main.tsx
@@ -1,10 +1,10 @@
 import Hero from '@/components/sections/Hero'
-import Roadmap from '@/components/sections/Roadmap'
+// import Roadmap from '@/components/sections/Roadmap'
 import Loader from '@/components/shared/Loader'
 import { Suspense } from 'react'
-import Vision from '@/components/sections/Vision'
-import Services from '@/components/sections/Services'
-import Workflows from '@/components/sections/Workflows'
+// import Vision from '@/components/sections/Vision'
+// import Services from '@/components/sections/Services'
+// import Workflows from '@/components/sections/Workflows'
 
 const Main = () => {
   return (
@@ -14,11 +14,11 @@ const Main = () => {
 
         <div className="w-full">
           <Hero />
-          <Workflows />
+          {/* <Workflows /> */}
           {/* <About /> */}
-          <Services />
+          {/* <Services />
           <Vision />
-          <Roadmap />
+          <Roadmap /> */}
 
           
         </div>
